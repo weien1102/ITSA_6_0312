4170d54a9acc71bcb409a0e77fd82609ed465ede
ien we <weien0900755550@gmail.com>
2023-03-12 03:47:40 +0800

    加入專案檔案。

---
+++ ITSA_6_0312/ITSA_6_0312.cpp
@@ -0,0 +1,65 @@
+﻿// ITSA_6_0312.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>  
+using namespace std;
+
+int main() {
+    int month;
+    while (cin >> month)
+    {
+        switch (month)
+        {
+        case 1:
+            cout << "冬天" << endl;
+            break;
+        case 2:
+            cout << "冬天" << endl;
+            break;
+        case 3:
+            cout << "春天" << endl;
+            break;
+        case 4:
+            cout << "春天" << endl;
+            break;
+        case 5:
+            cout << "春天" << endl;
+            break;
+        case 6:
+            cout << "夏天" << endl;
+            break;
+        case 7:
+            cout << "夏天" << endl;
+            break;
+        case 8:
+            cout << "夏天" << endl;
+            break;
+        case 9:
+            cout << "秋天" << endl;
+            break;
+        case 10:
+            cout << "秋天" << endl;
+            break;
+        case 11:
+            cout << "秋天" << endl;
+            break;
+        case 12:
+            cout << "冬天" << endl;
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
